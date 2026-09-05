0a1ac7b708cdf175f27e3a2c5d78c6d5bd42135a
SuperDuu <vuducdu2k5@gmail.com>
2026-09-05 09:05:59 +0700

    add cal prototypes

--- firmware/joint_driver/joint-driver-8115/Core/Inc/encoder_cal_store.h
+++ firmware/joint_driver/joint-driver-8115/Core/Inc/encoder_cal_store.h
@@ -5,7 +5,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-bool EncoderCalStore_Load(AS5048A_t *encoder);
-bool EncoderCalStore_Save(const int16_t lut[AS5048A_LUT_SIZE]);
+bool EncoderCalStore_Load(AS5048A_t *encoder, float *zero_electric_angle, int8_t *encoder_dir, bool *is_aligned);
+bool EncoderCalStore_Save(const int16_t lut[AS5048A_LUT_SIZE], float zero_electric_angle, int8_t encoder_dir);
+bool EncoderCalStore_SaveAlignment(float zero_electric_angle, int8_t encoder_dir);
 
 #endif /* ENCODER_CAL_STORE_H */
